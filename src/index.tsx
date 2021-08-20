653d0a2c4ad6370cf87bec998923fdc51d7c5cf5
MelkdeSousa <melk.sousap@gmail.com>
2021-08-20 13:47:32 +0000

    feat: theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+
+import { theme } from './styles/theme'
 
 import App from './App'
 import GlobalStyle from './styles/global'
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <GlobalStyle />
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/styles/theme.ts
@@ -0,0 +1,6 @@
+export const theme = {
+    background: '#9B8FE8',
+    primary: '#8373E8',
+    secondary: '#E0349B',
+    white: '#EFEDF9'
+}
