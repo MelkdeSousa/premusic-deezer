fdcfc1ae8663296fa6b39d0813159cc8caa59f95
MelkdeSousa <melk.sousap@gmail.com>
2021-08-20 17:42:16 +0000

    chore: spaces adjustment

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,8 @@
 export const theme = {
-    background: '#9B8FE8',
-    primary: '#8373E8',
-    secondary: '#E0349B',
-    white: '#EFEDF9'
+  background: '#9B8FE8',
+  primary: '#8373E8',
+  secondary: '#E0349B',
+  white: '#EFEDF9'
 }
 
 export type ThemeType = typeof theme
